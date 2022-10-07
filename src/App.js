1d13d34ce45476abde69e11f6a386ed3fdcd20fc
tysonmedora6 <44410848+tysonmedora6@users.noreply.github.com>
2022-10-07 19:20:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,16 +87,18 @@ loadUser = (data) => {
 }
 
 calculateFaceLocation = (data) => {
-const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+const clarifaiFace = data.outputs[n].data.regions[n].region_info.bounding_box
 const image = document.getElementById('inputimage');
 const width = Number(image.width);
 const height = Number(image.height);
-return {
+for(n=0;n<5;n++){
+    return {
   leftCol: clarifaiFace.left_col * width,
   topRow: clarifaiFace.top_row * height,
   rightCol: width - (clarifaiFace.right_col * width),
   bottomRow: height - (clarifaiFace.bottom_row * height)
   }
+}
 }
 
 displayFaceBox = (box) => {
